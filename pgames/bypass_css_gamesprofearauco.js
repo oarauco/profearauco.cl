74b73bee1a537fe342129744279b259ad2829fb8
oarauco <163372041+oarauco@users.noreply.github.com>
2026-05-03 14:13:50 -0400

    Delete pgames/bypass_css_gamesprofearauco.js

--- pgames/bypass_css_gamesprofearauco.js
+++
@@ -1,6 +0,0 @@
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = 'https://cdn.jsdelivr.net/gh/oarauco/profearauco.cl/pgames/bypass_css_gamesprofearauco.js';
-document.head.appendChild(link);
-
-console.log("CSS inyectado dinámicamente");
