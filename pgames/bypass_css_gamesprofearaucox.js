81c03e8f4dbc6a6412ac0d18df8ac74fe1052c9d
oarauco <163372041+oarauco@users.noreply.github.com>
2026-05-03 14:27:55 -0400

    Add files via upload

---
+++ pgames/bypass_css_gamesprofearaucox.js
@@ -0,0 +1,6 @@
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://cdn.jsdelivr.net/gh/oarauco/profearauco.cl/pgames/gamesprofearauco.css';
+document.head.appendChild(link);
+
+console.log("CSS inyectado dinámicamente");
